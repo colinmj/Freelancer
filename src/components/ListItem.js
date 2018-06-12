a9a99f8dacc9aeddb02ee0c7ab9f20e9a261ffe6
Colin MJ <colinmj@telus.net>
2018-06-11 19:45:05 -0700

    Created env variables for firebase config

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import moment from 'moment';
+import numeral from 'numeral';
 
 const ListItem = ({ title, amount, created, description }) => {
   return (
     <div>
       <h4>{title} </h4>
-      <p>{amount}</p>
-      <p>{created}</p>
+      <p>{numeral(amount / 100).format('$0,0.00')} </p>
+      <p>{moment(created).format('MMMM Do, YYYY')}</p>
       <p>{description}</p>
     </div>
   );
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,16 +2,14 @@ import * as firebase from 'firebase';
 
 // what this does is it takes all of the named exports and calls it firebase
 
-var config = {
-  apiKey: 'AIzaSyAmu6YccqdRgBRveLWiuMwPJszabwmB7iA',
-  authDomain: 'freelancer-fd087.firebaseapp.com',
-  databaseURL: 'https://freelancer-fd087.firebaseio.com',
-  projectId: 'freelancer-fd087',
-  storageBucket: '',
-  messagingSenderId: '652530607486'
+const config = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
-
-console.log(process.env);
 
 firebase.initializeApp(config);
 
