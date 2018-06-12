68fc03b5a7d017fa2fe19451d53a8a77c62af172
Colin MJ <colinmj@telus.net>
2018-06-11 20:31:16 -0700

    Created environment variables

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,12 +3,12 @@ import * as firebase from 'firebase';
 // what this does is it takes all of the named exports and calls it firebase
 
 const config = {
-  apiKey: 'AIzaSyAmu6YccqdRgBRveLWiuMwPJszabwmB7iA',
-  authDomain: 'freelancer-fd087.firebaseapp.com',
-  databaseURL: 'https://freelancer-fd087.firebaseio.com',
-  projectId: 'freelancer-fd087',
-  storageBucket: 'freelancer-fd087.appspot.com',
-  messagingSenderId: '652530607486'
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
@@ -19,10 +19,3 @@ const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
 const facebookProvider = new firebase.auth.FacebookAuthProvider();
 
 export { firebase, googleAuthProvider, facebookProvider, db as default };
-
-// FIREBASE_API_KEY=AIzaSyAmu6YccqdRgBRveLWiuMwPJszabwmB7iA
-// FIREBASE_AUTH_DOMAIN=freelancer-fd087.firebaseapp.com
-// FIREBASE_DATABASE_URL=https://freelancer-fd087.firebaseio.com
-// FIREBASE_PROJECT_ID=freelancer-fd087
-// FIREBASE_STORAGE-BUCKET=freelancer-fd087.appspot.com
-// FIREBASE_MESSAGING_SENDER_ID=652530607486
