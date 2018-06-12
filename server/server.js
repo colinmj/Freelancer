c2132194433ffa975258dfef739a4f0e85313464
Colin MJ <colinmj@telus.net>
2018-06-11 19:50:24 -0700

    Edited Server

--- server/server.js
+++ server/server.js
@@ -4,8 +4,6 @@ const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
 const port = process.env.PORT || 3000;
 
-console.log(process.env);
-
 //middleware, something that runs for each request
 app.use(express.static(publicPath));
 
